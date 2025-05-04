b734acaf178271c534fb58db44e84047849f68c1
palakshah-47 <palakshah47@gmail.com>
2025-05-03 22:49:10 -0400

    fix: fix for products route

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,12 +2,18 @@ import { NextRequest, NextResponse } from 'next/server';
 import { fetchProductsByCategory } from '@/actions/fetchProducts';
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams } = request.nextUrl;
   const category =
     searchParams.get('category') ?? undefined;
   console.log('category in route', category);
   const query = searchParams.get('q') ?? undefined;
   console.log('query in route', query);
+  if (!category) {
+    return NextResponse.json(
+      { error: 'Missing category' },
+      { status: 400 },
+    );
+  }
   const products = await fetchProductsByCategory({
     category: query && query !== '' ? undefined : category,
     query: category && category !== '' ? query : undefined,
