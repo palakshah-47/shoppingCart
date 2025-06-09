3a7c7a4f6ab0a1fc40ad6dc5fbb4e2809e56a63f
palakshah-47 <palakshah47@gmail.com>
2025-06-09 15:22:44 -0400

    fix: adding authentication for a route

--- src/app/products/api/revalidate-all-products/route.ts
+++ src/app/products/api/revalidate-all-products/route.ts
@@ -2,7 +2,17 @@ import { revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
 import prisma from '@/libs/prismadb';
 
-export async function POST() {
+export async function POST(req: Request) {
+  const authHeader = req.headers.get('authorization');
+  if (
+    authHeader !== `Bearer ${process.env.REVALIDATE_SECRET}`
+  ) {
+    return NextResponse.json(
+      { error: 'Unauthorized' },
+      { status: 401 },
+    );
+  }
+
   const allProducts = await prisma.product.findMany({
     select: { id: true },
   });
