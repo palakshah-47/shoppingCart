08f0cb5e15bf71eec77b18ac61b256b5c48e8b85
palakshah-47 <palakshah47@gmail.com>
2025-07-14 12:06:56 -0400

    fix: fixing image size and removing memo

--- src/app/components/products/ProductImage.tsx
+++ src/app/components/products/ProductImage.tsx
@@ -1,6 +1,9 @@
 'use client';
 
-import { CartProductType, SelectedImgType } from '@/app/products/[productId]/ProductDetails';
+import {
+  CartProductType,
+  SelectedImgType,
+} from '@/app/products/[productId]/ProductDetails';
 import Image from 'next/image';
 import { Product } from './types';
 
@@ -10,7 +13,11 @@ interface ProductImageProps {
   handleColorSelect: (value: SelectedImgType) => void;
 }
 
-const ProductImage: React.FC<ProductImageProps> = ({ cartProduct, product, handleColorSelect }) => {
+const ProductImage: React.FC<ProductImageProps> = ({
+  cartProduct,
+  product,
+  handleColorSelect,
+}) => {
   return (
     <div
       className="grid grid-cols-6
@@ -32,26 +39,28 @@ const ProductImage: React.FC<ProductImageProps> = ({ cartProduct, product, handl
       max-h-[500px]
       min-h-[300px]
       sm-min-h-[400px]">
-        {product?.images.map((image: SelectedImgType, index: number) => {
-          return (
-            <div
-              key={`${image.alt} - ${index}`}
-              onClick={() => handleColorSelect(image)}
-              className={`relative w-[80%]
+        {product?.images.map(
+          (image: SelectedImgType, index: number) => {
+            return (
+              <div
+                key={`${image.alt} - ${index}`}
+                onClick={() => handleColorSelect(image)}
+                className={`relative w-[80%]
             aspect-square rounded border-teal-300 
             ${cartProduct?.selectedImg?.alt === image?.alt ? 'border-[1.5px]' : 'border: none'}`}>
-              <Image
-                src={image.image}
-                alt={`${product.title} - Image ${index + 1}`}
-                fill
-                className="object-contain"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                priority={false}
-                loading="lazy"
-              />
-            </div>
-          );
-        })}
+                <Image
+                  src={image.image}
+                  alt={`${product.title} - Image ${index + 1}`}
+                  fill
+                  className="object-contain"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  priority={false}
+                  loading="lazy"
+                />
+              </div>
+            );
+          },
+        )}
       </div>
       <div className="col-span-5 relative aspect-square">
         <Image
@@ -59,7 +68,7 @@ const ProductImage: React.FC<ProductImageProps> = ({ cartProduct, product, handl
           src={cartProduct?.selectedImg?.image ?? ''}
           alt={cartProduct?.name ?? ''}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="w-full h-full object-contain max-h-[500px] min-h-[300px] sm:min-h-[400px]"
+          className="object-contain max-h-[500px] min-h-[300px] sm:min-h-[400px]"
           priority={false}
           loading="lazy"
         />{' '}
